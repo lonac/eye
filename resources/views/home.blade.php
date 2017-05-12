ddac773dfe57533f17a84b0060e2fb6ae13ca194
lonac <barakamalonao@yahoo.com>
2017-05-12 14:21:21 +0300

    modified:   app/Http/Controllers/HomeController.php modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.master')
 
-@section('title','home')
+@section('title','Account')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+        <div class="col-md-12 col-md-offset-0">
+            <div class="panel panel-success">
+                <div class="panel-heading">Welcome {{ Auth::user()->firstname}} {{ Auth::user()->lastname}}</div>
                 <div class="panel-body">
-                    You are logged in!
+                  
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+
         </div>
     </div>
 </div>
