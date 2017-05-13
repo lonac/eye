cc6a41669052f5986e35f301b90aaa1c9226ae68
lonac <barakamalonao@yahoo.com>
2017-05-13 17:51:58 +0300

    app/Http/Controllers/CategoryController.php resources/views/categories/

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('title', 'Add Category')
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h3 class="panel-title"> College Categories:</h3>
+				</div>
+				<div class="panel-body">
+					
+					<form method="POST" action="{{ url('categories/create') }}">
+
+						{{ csrf_field() }}
+
+						<div class="form-group">
+							<label for="colgcategories">College Name</label>
+							<input type="text" name="cat_name" id="category" class="form-control" placeholder="categories e.g Administration" />
+						</div>
+						<button type="submit" class="btn btn-success">ADD</button>
+					</form>
+
+				</div>
+			</div>
+		</div>		
+
+	</div>
+</div>
+
+@endsection
