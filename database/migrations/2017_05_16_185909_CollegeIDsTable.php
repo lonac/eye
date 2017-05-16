14ac5b0c1b8da2746f5b9cbe679ab27c6e53ec69
lonac <barakamalonao@yahoo.com>
2017-05-16 22:05:56 +0300

    database/migrations/2017_05_16_185909_CollegeIDsTable.php

---
+++ database/migrations/2017_05_16_185909_CollegeIDsTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CollegeIDsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collegestaffids',function( Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('college_id')->unsigned()->index();
+            $table->foreign('college_id')->references('id')->on('colleges')->onUpdate('cascade');
+            $table->string('staffids');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collegestaffids');
+    }
+}
