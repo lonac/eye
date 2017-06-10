6c2d9de539fa82e6841a3772c1106d68a044cbe0
lonac <barakamalonao@yahoo.com>
2017-06-10 20:37:55 +0300

    modified:   resources/views/layouts/master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
     @yield('content')
 
     <footer class="row">
-        @include('layouts.footer')
+        {{--@include('layouts.footer')--}}
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
