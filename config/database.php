ef267a5f0b77f1535cf19e1c9b3ecc444c389ffc
lonac <barakamalonao@yahoo.com>
2017-12-14 20:58:12 +0300

    db ammended

--- config/database.php
+++ config/database.php
@@ -53,19 +53,16 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+           /* 'pgsql' => [
+                'driver'   => 'pgsql',
+                'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+                'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+                'username' => parse_url(getenv("DATABASE_URL"))["user"],
+                'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+                'charset'  => 'utf8',
+                'prefix'   => '',
+                'schema'   => 'public',
+            ], */
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
@@ -77,6 +74,8 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
+
+
 
     ],
 
